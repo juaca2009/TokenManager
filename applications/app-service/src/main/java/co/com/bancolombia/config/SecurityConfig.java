493cc31f92d115baf79dd8285da60c01367d59e9
andfesal <andfesal@bancolombia.com.co>
2024-11-24 11:07:21 -0500

    Clean Arquitecture set with import errors

---
+++ applications/app-service/src/main/java/co/com/bancolombia/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package co.com.bancolombia.config;
+
+/*import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration*/
+public class SecurityConfig {
+    /*@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable() // Deshabilitar CSRF para pruebas
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/login").permitAll() // Permitir acceso público a /auth/login
+                .anyRequest().authenticated(); // Requerir autenticación para otras rutas
+
+        return http.build();
+    }*/
+}
---
+++ applications/app-service/src/main/java/co/com/bancolombia/config/controller/AuthController.java
@@ -0,0 +1,46 @@
+package co.com.bancolombia.config.controller;
+
+import lombok.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    /*@PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        // Lógica de autenticación básica
+        if ("user".equals(loginRequest.getUsername()) && "password".equals(loginRequest.getPassword())) {
+            return ResponseEntity.ok("Login exitoso!");
+        } else {
+            return ResponseEntity.status(401).body("Credenciales inválidas");
+        }
+    }
+
+    // Clase estática para manejar las credenciales de inicio de sesión
+    public static class LoginRequest {
+        private String username;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }*/
+
+}
